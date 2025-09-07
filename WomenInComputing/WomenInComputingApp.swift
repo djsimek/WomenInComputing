20e6d1e357bc514ea23c214c5f048e6533c8f5a0
Dan Simek <djsimek@yahoo.com>
2025-09-06 22:38:56 -0500

    Initial Commit

---
+++ WomenInComputing/WomenInComputingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WomenInComputingApp.swift
+//  WomenInComputing
+//
+//  Created by Dan Simek on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct WomenInComputingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
